16d64b3f356bc1eac58a2d689727316d0d3f410c
AriesMolinar22 <109078921+AriesMolinar22@users.noreply.github.com>
2024-09-12 13:48:47 +0800

    initial commit

---
+++ script.js
@@ -0,0 +1,20 @@
+var countryElements = document.getElementById('countries').childNodes;
+    var countryCount = countryElements.length;
+    for (var i = 0; i < countryCount; i++) {
+      countryElements[i].onclick = function() {
+        alert('You clicked on ' + this.getAttribute('data-name'));
+        console.log(this.getAttribute('data-name'))
+     
+        if ( this.getAttribute('data-name') == "Philippines") {
+            window.location = ('https://my.matterport.com/show/?m=e47xxQRgvjp') 
+        }else if ( this.getAttribute('data-name') == "United States"){
+            window.location = ('https://my.matterport.com/show/?m=JAxyUNbfEKH') 
+        }else if ( this.getAttribute('data-name') == "Japan"){
+            window.location = (' https://my.matterport.com/show/?m=SX1oSDauzXf') 
+        }else {
+            window.location = ('http://google.com') 
+        }
+      }
+    }
+
+   
